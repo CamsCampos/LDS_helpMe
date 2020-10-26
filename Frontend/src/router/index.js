cf1948afa1ad9f0da5d6c4f8613daf9944aa267a
lucasnoman <lucas.noman7@gmail.com>
2020-10-26 12:36:49 -0300

    Solicitar aula

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,29 +1,43 @@
 // ##### Importações #####
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
 // ** importações de componentes **
-import Cadastro from '@/views/CadastroAula.vue'
-import CadastroUsuario from '@/views/CadastroUsuario.vue'
-import PaginaEmBranco from '@/components/templates/PaginaEmBranco'
+import Cadastro from "@/views/CadastroAula.vue";
+import CadastroUsuario from "@/views/CadastroUsuario.vue";
+import PaginaEmBranco from "@/components/templates/PaginaEmBranco";
+import SolicitaAula from "../components/SolicitarAulas";
+import Card from "../components/templates/Card";
 
 // ##### Usar plugins de importações #####
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // ##### Atribuir importações #####
 export default new VueRouter({
-    mode: 'history',
-    routes: [{
-        path: '/',
-        component: PaginaEmBranco
-    }, {
-        path: '/cadastro',
-        component: Cadastro
-    }, {
-        path: '/cadastro-usuario',
-        component: CadastroUsuario
-    }]
-})
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: PaginaEmBranco,
+    },
+    {
+      path: "/cadastro",
+      component: Cadastro,
+    },
+    {
+      path: "/cadastro-usuario",
+      component: CadastroUsuario,
+    },
+    {
+      path: "/solicita-aula",
+      component: SolicitaAula,
+    },
+    {
+      path: "/Card",
+      component: Card,
+    },
+  ],
+});
 
 // const routes = [{
 //     path: '/',
